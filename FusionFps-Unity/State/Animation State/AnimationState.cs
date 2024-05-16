ab4838cea9239eb0628157f7eb9345325db5695b
LastCeri <125711498+LastCeri@users.noreply.github.com>
2024-05-17 02:22:37 +0300

    Add Unity Game [State]

---
+++ FusionFps-Unity/State/Animation State/AnimationState.cs
@@ -0,0 +1,16 @@
+namespace FusionFPS.States {
+    public enum AnimationState {
+        Idle,
+        Walk,
+        Run,
+        Sprint,
+        Jump,
+        Attack,
+        Die,
+        RotateLeft,
+        RotateRight,
+        SingleAttack,
+        MultiAttack,
+        Reload
+    }
+}
